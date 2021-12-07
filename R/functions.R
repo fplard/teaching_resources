4fc96d26f481abcc0531429e07ecd7dd18d03865
fplard <floriane.c.plard@gmail.com>
2021-12-07 16:01:30 +0100

    update basics

--- R/functions.R
+++ R/functions.R
@@ -2,7 +2,7 @@
 make_icon <- function(icon) {
   return(htmltools::tag("i", list(class = icon)))
 }
-ilink <- function(icon = NULL, url = NULL,lvl=2) {
+ilink <- function(icon = NULL, url = NULL,lvl=0) {
   icon_name=switch(icon,
          site="fas fa-globe",
          article="far fa-sticky-note",
@@ -13,8 +13,5 @@ ilink <- function(icon = NULL, url = NULL,lvl=2) {
          exo="fas fa-edit",
          github="fab fa-github")
   text <- htmltools::HTML(paste0(make_icon(icon_name)))
-  if(lvl==1) return(htmltools::a(href = url, text, class = "iconlink1"))
-  if(lvl==2) return(htmltools::a(href = url, text, class = "iconlink2"))
-  if(lvl==3) return(htmltools::a(href = url, text, class = "iconlink3"))
+  return(htmltools::a(href = url, text, class = paste("iconlink",lvl,sep="")))
 }
-#
